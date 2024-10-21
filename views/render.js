6f1bb502c72f7cb7c044de249f724338c4fe704d
Albina Karimova <karimova.albin@gmail.com>
2024-10-21 13:12:08 +0200

    Add generated output.html file

---
+++ views/render.js
@@ -0,0 +1,28 @@
+import ejs from 'ejs';
+import { writeFileSync } from 'fs';
+
+// Example posts data
+const posts = [
+    {
+        title: 'First Post',
+        date: 'October 20, 2024',
+        cardContent: 'This is a summary of the first post.'
+    },
+    {
+        title: 'Second Post',
+        date: 'October 21, 2024',
+        cardContent: 'This is a summary of the second post.'
+    }
+];
+
+// Render the EJS template with the posts data
+ejs.renderFile('template.ejs', { posts }, (err, html) => {
+    if (err) {
+        console.error('Error rendering the EJS file:', err);
+        return;
+    }
+
+    // Write the rendered HTML to output.html
+    writeFileSync('output.html', html);
+    console.log('HTML file successfully created!');
+});
